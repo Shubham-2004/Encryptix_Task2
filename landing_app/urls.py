e2439e5a04a605b428dfac9af8d196ab29bd8a08
Shubham-2004 <shubhambera100@gmail.com>
2024-05-11 14:08:21 +0530

    second commit

--- landing_app/urls.py
+++ landing_app/urls.py
@@ -1,6 +1,11 @@
 from django.urls import path,include
 from .import views
 
+from django.conf import settings
+from django.conf.urls.static import static
+
 urlpatterns = [
     path('',views.Landing,name='landing')
 ]
+if settings.DEBUG:
+    urlpatterns += static(settings.STATIC_URL, document_root=settings.STATIC_ROOT)
